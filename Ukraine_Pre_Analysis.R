88082ff8a0d94cc932db08c64f56530b58e06755
will <sopwill3@gmail.com>
2022-06-09 11:25:39 -0400

    Shifted to Robust Standard Error

--- Ukraine_Pre_Analysis.R
+++ Ukraine_Pre_Analysis.R
@@ -1,8 +1,16 @@
+# # Installs
+# install.packages("ggplot2")
+# install.packages("dplyr")
+# install.packages("broom")
+# install.packages("ggpubr")
+# install.packages("estimatr")
+
 # Imports
 library(ggplot2)
 library(dplyr)
 library(broom)
 library(ggpubr)
+library(estimatr)
 
 # Get the data
 data = read.csv('./Dummy_Data_1.csv')
@@ -16,26 +24,26 @@ summary(treatment_group)
 
 
 # Perform Regression
-direct_model = lm(treatment ~ direct, data=data)
-indirect_model = lm(treatment ~ indirect, data=data)
-economic_model = lm(treatment ~ economic, data=data)
-political_model = lm(treatment ~ political, data=data)
-general_model = lm(treatment ~ general, data=data)
+direct_model = lm_robust(treatment ~ direct, data=data)
+indirect_model = lm_robust(treatment ~ indirect, data=data)
+economic_model = lm_robust(treatment ~ economic, data=data)
+political_model = lm_robust(treatment ~ political, data=data)
+general_model = lm_robust(treatment ~ general, data=data)
 
 
 # ...With Warfare Control 
-direct_w_model = lm(treatment ~ direct + warfare, data=data)
-indirect_w_model = lm(treatment ~ indirect + warfare, data=data)
-economic_w_model = lm(treatment ~ economic + warfare, data=data)
-political_w_model = lm(treatment ~ political + warfare, data=data)
-general_w_model = lm(treatment ~ general + warfare, data=data)
+direct_w_model = lm_robust(treatment ~ direct + warfare, data=data)
+indirect_w_model = lm_robust(treatment ~ indirect + warfare, data=data)
+economic_w_model = lm_robust(treatment ~ economic + warfare, data=data)
+political_w_model = lm_robust(treatment ~ political + warfare, data=data)
+general_w_model = lm_robust(treatment ~ general + warfare, data=data)
 
 # ...With Russia Control 
-direct_wr_model = lm(treatment ~ direct + warfare + russia, data=data)
-indirect_wr_model = lm(treatment ~ indirect + warfare + russia, data=data)
-economic_wr_model = lm(treatment ~ economic + warfare + russia, data=data)
-political_wr_model = lm(treatment ~ political + warfare + russia, data=data)
-general_wr_model = lm(treatment ~ general + warfare + russia, data=data)
+direct_wr_model = lm_robust(treatment ~ direct + warfare + russia, data=data)
+indirect_wr_model = lm_robust(treatment ~ indirect + warfare + russia, data=data)
+economic_wr_model = lm_robust(treatment ~ economic + warfare + russia, data=data)
+political_wr_model = lm_robust(treatment ~ political + warfare + russia, data=data)
+general_wr_model = lm_robust(treatment ~ general + warfare + russia, data=data)
 
 
 print(summary(direct_wr_model), digits=3)
@@ -45,6 +53,9 @@ print(summary(political_wr_model), digits=3)
 print(summary(general_wr_model), digits=3)
 
 # Calculate Significance
+
+
+## USING ROBUST STANDARD ERRORS
 
 
 

c3dc0d0144bc6a557c7f4fd455f8b37c07e9fea8
will <sopwill3@gmail.com>
2022-06-22 16:22:45 -0600

    did rough power calculation, still need to refine estimates and make sure simulation makes sense for multiple treatments

--- Power-Calculations.R
+++ Power-Calculations.R
@@ -1,27 +1,67 @@
 # Power Calculations
 # https://egap.org/resource/10-things-to-know-about-statistical-power/
 
-
-# Assumptions
-COST_PER_RESPONSE = 1
-# How statistically significant must the results be?
-BIG_ALPHA = 0.05
-SMALL_ALPHA = 0.01
-# How large am I assuming the treatment effect to be?
+# KEY ASSUMPTIONS
+COST_PER_RESPONSE = 0.80 
+  # Based on GA minimum wage for a 5 minute survey + 0.2 cents just for fun
+ALPHA = 0.01
 TREATMENT_EFFECT_SIZE = 0.3
-# How often will the experiment return statistically significant results?
 TARGET_POWER = 0.8
-AVERAGE_OUTCOME_CONTROL = 2.12
-AVERAGE_OUTCOME_TREATED = 2.72
+AVERAGE_OUTCOME = 2.5
 STANDARD_DEVIATION = 1.47
-SAMPLE_SIZE = 1000
 
+# helpers
+make_even <- function(x){
+  if(x %% 2 != 0){
+    x <- x + 1
+  }
+  return(x)
+}
 
-power_calculator <- function(mu_t, mu_c, sigma, alpha=0.05, N){ 
-  lowertail <- (abs(mu_t - mu_c)*sqrt(N))/(2*sigma) 
-  uppertail <- -1*lowertail 
-  beta <- pnorm(lowertail- qnorm(1-alpha/2), lower.tail=TRUE) + 1- pnorm(uppertail- qnorm(1-alpha/2), lower.tail=FALSE) 
-  return(beta) 
+# RUNNING SIMULATED EXPERIMENTS
+# possible.ns <- seq(from=100, to=2000, by=100) # The sample sizes we'll be considering
+possible.ns <- seq(from=100, to=2000, by=100) * 0.66
+possible.ns <- modify(possible.ns, make_even)
+possible.ns
+stopifnot(all( (possible.ns %% 2)==0 )) ## require even number of experimental pool
+powers <- rep(NA, length(possible.ns)) # Empty object to collect simulation estimates 
+alpha <- ALPHA # Standard significance level 
+sims <- 500 # Number of simulations to conduct for each N 
+#### Outer loop to vary the number of subjects #### 
+for (j in 1:length(possible.ns)){
+  N <- possible.ns[j] # Pick the jth value for N 
+  Y0 <- rnorm(n=N, mean=60, sd=20) # control potential outcome 
+  tau <- 5 # Hypothesize treatment effect 
+  Y1 <- Y0 + tau # treatment potential outcome                                   
+  significant.experiments <- rep(NA, sims) # Empty object to count significant experiments 
+  
+  #### Inner loop to conduct experiments "sims" times over for each N #### 
+  Y0 <- rnorm(n=N, mean=AVERAGE_OUTCOME, sd=STANDARD_DEVIATION) # control potential outcome 
+  tau <- TREATMENT_EFFECT_SIZE # Hypothesize treatment effect 
+  Y1 <- Y0 + tau # treatment potential outcome 
+    for (i in 1:sims){ 
+      ## Z.sim <- rbinom(n=N, size=1, prob=.5) # Do a random assignment  by coin flip
+      Z.sim <- sample(rep(c(0,1),N/2)) ## Do a random assignment ensuring equal sized groups
+      Y.sim <- Y1*Z.sim + Y0*(1-Z.sim) # Reveal outcomes according to assignment 
+      fit.sim <- lm(Y.sim ~ Z.sim) # Do analysis (Simple regression) 
+      p.value <- summary(fit.sim)$coefficients[2,4] # Extract p-values 
+      significant.experiments[i] <- (p.value <= alpha) # Determine significance according to p <= 0.05
+    }
+  powers[j] <- mean(significant.experiments) # store average success rate (power) for each N 
 } 
 
-power_calculator(AVERAGE_OUTCOME_TREATED, AVERAGE_OUTCOME_CONTROL, STANDARD_DEVIATION, alpha=SMALL_ALPHA, SAMPLE_SIZE)
+powers_df = data.frame(
+  sample_size = possible.ns,
+  power = powers,
+  cost = possible.ns * COST_PER_RESPONSE
+  )
+
+powers_df
+
+
+# Next steps are to refine the simulation so that it works with multiple treatments
+# Or you could just do what you were doing and assume 0.66 treatment size
+# 
+# Then you really need to refine your assumptions more...
+#   1. Look for effect sizes from similar framing experiments to cite
+#   2. Look ar IR public opinion studies to try to estimate standard deviation 
